42b2e97d6bc8dfd1f47306bb76a8948d961a22c9
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-13 17:05:55 +0300

    Initial Commit

---
+++ Welcomer/Config.cs
@@ -0,0 +1,12 @@
+﻿using Exiled.API.Interfaces;
+
+namespace Welcomer
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public string PlayerWelcome { get; set; } = "Welcome to the server";
+        public ushort Duration { get; set; } = 5;
+    }
+}
---
+++ Welcomer/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.API.Features;
+
+namespace Welcomer
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin instance;
+        public override void OnEnabled()
+        {
+            instance = this;
+            Exiled.Events.Handlers.Player.Joined += PlayerHandler.PlayerJoined;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Joined -= PlayerHandler.PlayerJoined;
+            base.OnDisabled();
+        }
+    }
+}
